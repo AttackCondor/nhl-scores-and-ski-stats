ea3b56d316da931a2ecb22829d3fe5c098adf285
WiCoKo <wmconnorkoch@gmail.com>
2021-11-08 14:15:05 -0800

    typo

--- src/api/mountain/mountaindata.py
+++ src/api/mountain/mountaindata.py
@@ -17,7 +17,7 @@ def get_mountain_forecast(mountain_id, app_id, app_key, hourly_interval = None, 
     try:
         #attempt to retrieve the forecast, if hourly interval is not provided default to 12, if num_of_days is not provided default to 1
         data = requests.get(
-            FORMAT_URL.format("https://api.weatherunlocked.com/api/resortforecast/619002?app_id=eb227ae7&app_key=dc723588891a6e17a9ab088f150fd38f&num_of_days=1", 
+            "https://api.weatherunlocked.com/api/resortforecast/619002?app_id=eb227ae7&app_key=dc723588891a6e17a9ab088f150fd38f&num_of_days=1", 
             timeout=REQUEST_TIMEOUT
         )
     except requests.exceptions.RequestException as e:
